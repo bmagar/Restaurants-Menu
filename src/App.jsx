f886fda9b2147004bbc5e0f72870e0c01c5e85ee
Your Namebmagar <you@example.commagarbinod5@gmail.com>
2023-01-15 17:43:48 -0600

    first Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,38 @@
 import React from "react";
+import About from "./About";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
-const App = () => {
+import { Fragment } from "react";
+
+function App() {
+  const theme = {
+    colors: {
+      heading: "rgb(24 24 29)",
+      text: "rgb(24 24 29)",
+      white: "#fff",
+      helper: "#212529",
+      bg: "rgb(249 249 255)",
+      footer_bg: "#0a1435",
+      btn: "rgb(98 84 243",
+      border: "rgba(98, 84, 243, 0.6)",
+      hr: "#ffffff",
+      gradient:
+        "linear-gradient(0deg, rgb(132 144 255) 0%, rgb(98 189 252) 100%",
+    },
+    media: { mobile: "768px", tab: "998px" },
+  };
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          Navbar
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Link
-              </a>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false">
-                Dropdown
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <hr class="dropdown-divider" />
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a
-                class="nav-link disabled"
-                href="#"
-                tabindex="-1"
-                aria-disabled="true">
-                Disabled
-              </a>
-            </li>
-          </ul>
-          <form class="d-flex">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
-        </div>
-      </div>
-    </nav>
+    <Fragment>
+      <ThemeProvider theme={theme}>
+        <About />
+      </ThemeProvider>
+    </Fragment>
   );
-};
+}
 
 export default App;
+
+// Style Component
+// -> theme.provider
+// -> Create a const theme
