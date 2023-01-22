a92a107ab85dba40d7a06dc2eb4923797196d18d
Your Namebmagar <you@example.commagarbinod5@gmail.com>
2023-01-21 23:39:01 -0600

    adding function

--- src/components/Resturant.jsx
+++ src/components/Resturant.jsx
@@ -5,8 +5,44 @@ import MenuCard from "./MenuCard";
 
 const Resturant = () => {
   const [menuData, setMenuData] = useState(Menu);
+
+  // adding function for navbar button
+  const filterItem = (hello) => {
+    const updatedList = Menu.filter((curElem) => {
+      return curElem.category === hello;
+    });
+    setMenuData(updatedList);
+  };
+
   return (
     <>
+      <nav className="navbar container mt-5">
+        <div className="btn-group">
+          <button
+            className="btn-group-item"
+            onClick={() => filterItem("appetizer")}>
+            Appetizer
+          </button>
+          <button
+            className="btn-group-item"
+            onClick={() => filterItem("dishes")}>
+            Dishes
+          </button>
+          <button
+            className="btn-group-item"
+            onClick={() => filterItem("biryani")}>
+            Biryani
+          </button>
+          <button
+            className="btn-group-item"
+            onClick={() => filterItem("chicken specialist")}>
+            Curry
+          </button>
+          <button className="btn-group-item" onClick={() => setMenuData(Menu)}>
+            All
+          </button>
+        </div>
+      </nav>
       <MenuCard menuData={menuData} />
     </>
   );
